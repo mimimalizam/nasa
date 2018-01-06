247a4145b4ae67f705930392d141030e954979dd
Milana <milana.stojadinov@gmail.com>
2018-01-06 13:19:04 +0100

    Ping Houston

--- web.rb
+++ web.rb
@@ -1,9 +1,24 @@
 require "sinatra/base"
+require "httparty"
+require "json"
 
 module Nasa
   class Web < Sinatra::Base
+
     get "/" do
       "Space is good for you"
     end
+
+    post "/houston" do
+      respond_message(
+        "Roger. Nasa will show you photo, just be nice please."
+      )
+    end
+
+    def respond_message(message)
+      content_type :json
+      { :text => message }.to_json
+    end
+
   end
 end
